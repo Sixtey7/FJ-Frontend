d279403a47eaabb56e4d675448b7367b72c92908
Patrick Shaw <sixtey7@gmail.com>
2019-02-18 22:01:21 -0700

    start working on a Vue Component for transactions

---
+++ src/components/TransactionEntry.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class = "trans-item">
+        <h3>{{ name }}</h3>
+        <p>Account \${{ amount }}</p>
+        <input :value = "amount" type = "number" @input="$emit('update:amount', parseInt($event.target.value))">
+        <p>Type: {{ type }}</p>
+        <div v-if="date"><p>Date: {{ date }}</p></div>
+        <div v-if="notes"><p>Notes: {{ notes }}</p></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TransactionEntry',
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        amount: {
+            type: Number,
+            default: 0
+        },
+        type: {
+            type: String,
+            validator: function (value) {
+                return ['Planned', 'Estimate', 'Pending', 'Confirmed', 'Future'].indexOf(value) !== -1;
+            }
+        },
+        date: Date,
+        notes: String
+    }
+}
+</script>
