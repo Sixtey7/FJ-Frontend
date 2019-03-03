b46be8d112728901dcb2bb791767b54a6a009868
Patrick Shaw <sixtey7@gmail.com>
2019-03-03 12:06:47 -0500

    adjust the used format

--- src/components/DateCell.vue
+++ src/components/DateCell.vue
@@ -15,7 +15,7 @@ export default {
             if (!value) {
                 return;
             }
-            return moment(value).format('YYYY-MM-DD');
+            return moment(value).format('MM/DD/YY');
         }
     }
 }
