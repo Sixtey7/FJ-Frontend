2206a5b4c57baa1fbc3dced6d9bf0b0339091d6c
Patrick Shaw <sixtey7@gmail.com>
2019-03-30 12:23:29 -0400

    more detail in TransactionHelper

--- src/components/TransactionTable.vue
+++ src/components/TransactionTable.vue
@@ -15,7 +15,7 @@
                 <tr v-for = "transaction in transactions" :key = "transaction.id">
                     <StringCell :stringToShow="transaction.name" />
                     <MoneyCell :amount="transaction.amount" />
-                    <MoneyCell :amount=0 />
+                    <MoneyCell :amount="transaction.balance" />
                     <StringCell :stringToShow="transaction.type" />
                     <DateCell :dateToShow="transaction.date" />
                     <StringCell :stringToShow="transaction.notes" />
--- src/utils/TransactionHelper.js
+++ src/utils/TransactionHelper.js
@@ -6,11 +6,34 @@ class TransactionHelper {
             transactionList.forEach(transaction => {
                 transaction.date = new Date(transaction.date);
             });
+
+            transactionList.sort(this._compareTx);
         }
+
+        this._calculateBalance(transactionList);
 
 
         return transactionList;
     } 
+
+     _compareTx(a, b) {
+        if (a.date < b.date) {
+            //eslint-disable-next-line
+            console.log('Determined {} less than {}', a.date, b.date);
+            return -1;
+        }
+        else if (b.date < a.date) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    _calculateBalance(transactionList) {
+        transactionList.forEach(transaction => {
+            transaction.balance = 0;
+        })
+    }
 }
 
 export default TransactionHelper;
