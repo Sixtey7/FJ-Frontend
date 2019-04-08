99c016630401abc6a4a8685e49ca2eaa2c8f6d40
Patrick Shaw <sixtey7@gmail.com>
2019-04-07 21:48:53 -0400

    remove extra comment

--- src/components/NewTxModal.vue
+++ src/components/NewTxModal.vue
@@ -85,15 +85,6 @@
     border: 1px solid #ddd;
 }
 
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
 .modal-enter {
   opacity: 0;
 }
