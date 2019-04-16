1dcc6fe1fbb2c4e2441137369a92c8b4d0dc9720
Patrick Shaw <sixtey7@gmail.com>
2019-04-16 06:34:20 -0400

    add an edit button to each row

---
+++ src/components/ButtonCell.vue
@@ -0,0 +1,13 @@
+<template>
+    <td><button type="button" class = "btn btn-success">Edit</button></td>
+</template>
+
+<script>
+
+export default {
+    name: "ButtonCell",
+    props: {
+        id: String
+    }
+};
+</script>
