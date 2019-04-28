16be73f33f9da9a2659270c9179ea4921067d5ad
Patrick Shaw <sixtey7@gmail.com>
2019-04-28 10:34:49 -0400

    add guard code to MoneyCell

--- src/components/cells/MoneyCell.vue
+++ src/components/cells/MoneyCell.vue
@@ -17,6 +17,11 @@ export default {
     },
     computed: {
         amountToShow: function() {
+            //TODO: This guard code is likely eventually not needed, but today it is
+            if (this.amount === null) {
+                return (0).toLocaleString(undefined, options);
+            }
+
             if (this.amount < 0) {
                 return "(" + Math.abs(this.amount).toLocaleString(undefined, options) + ")";
             }
