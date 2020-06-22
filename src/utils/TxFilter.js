7946376c2904693b759d190ed2e6a1e0d3b79a44
Patrick Shaw <sixtey7@gmail.com>
2020-06-22 08:45:30 -0600

    more comments

--- src/utils/TxFilter.js
+++ src/utils/TxFilter.js
@@ -1,10 +1,18 @@
 import DateHelper from './DateHelper';
+/**
+ * Class used to hold the details of a filter applied to Transactions
+ */
 class TxFilter {
     isStartFilter;
     isEndFilter;
     startFilterDate;
     endFilterDate;
 
+    /**
+     * 
+     * @param {Date} startFilerDate the start of the filter (null if none)
+     * @param {Date} endFilterDate the end of the filter (null if none)
+     */
     constructor(startFilerDate, endFilterDate) {
         if (startFilerDate !== null) {
             this.isStartFilter = true;
@@ -25,6 +33,9 @@ class TxFilter {
         }
     }
 
+    /**
+     * Returns the start filter the user has applied as a Date object (null if none)
+     */
     startFilterAsDate() {
         if (this.isStartFilter) {
             return new Date(this.startFilterDate);
@@ -34,6 +45,9 @@ class TxFilter {
         }
     }
 
+    /**
+     * Returns the end filter the user has applied as a Date object (null if none)
+     */
     endFilterAsDate() {
         if (this.isEndFilter) {
             return new Date(this.endFilterDate);
@@ -43,6 +57,10 @@ class TxFilter {
         }
     }
 
+    /**
+     * Constructs a TxFilter object out of the provided string
+     * @param {String} json the TxFilter object as a json string
+     */
     static fromJSON(json) {
         return Object.assign(this, json);
     }
